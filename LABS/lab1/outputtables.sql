8c6b5c106bfe05bb54b238f153478b7f90fc321d
Who I am <118208855+Who-I-ammmmmmm@users.noreply.github.com>
2023-06-03 22:11:06 +0200

    Delete LABS/lab1 directory

--- LABS/lab1/outputtables.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT * FROM Employees;
-SELECT * FROM Departments;
-SELECT * FROM Positions;
-SELECT * FROM PositionHistory;
-SELECT * FROM DepartmentVacations;
-SELECT * FROM DepartmentEmployees;
-SELECT * FROM Reports;
